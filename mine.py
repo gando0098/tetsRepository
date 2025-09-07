9a7be6f93c3dc74641d1d931ab4e2adf4150027b
gando <kuchakzaiiiabubakr@gmail.com>
2025-09-07 10:23:37 +0330

    update code

--- mine.py
+++ mine.py
@@ -1,14 +1,18 @@
 import random
 
+randomnum = random.randint(1,10)
 
 while True:
     
-    randomnum = random.randint(1,10)
 
     guess = int(input("guess a number between 1 and 10 :"))
 
     if guess == randomnum :
         print("correct !")
+        break
+    elif guess > 10 or guess < 1:
+        print("enter a valible number")
+        
     elif guess < randomnum :
         print("your number is low")
     else:
